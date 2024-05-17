2916d416939ab07a6d03acb0850c64052d3c75dd
Der Robert <erictainment@live.de>
2024-05-18 00:24:01 +0200

    implementing tests

---
+++ SharpTools2024.UnitTests/SizeOfTests.cs
@@ -0,0 +1,15 @@
+﻿namespace SharpTools2024.UnitTests;
+
+using Xunit;
+using SharpTools2024;
+
+public class SizeOfTests
+{
+	[Fact]
+	public void BitsShouldStayBits()
+	{
+		var bits = SizeOf.Bits(12);
+		var result = bits.inBits();
+		Assert.Equal(12, result);
+	}
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿namespace SharpTools2024.UnitTests;
+
+using SharpTools2024;
+
+public class SizeOfTests
+{
+	[Test]
+	public void BitsShouldStayBits()
+	{
+		var bits = SizeOf.Bits(12);
+		var result = bits.inBits();
+		Assert.That(result, Is.EqualTo(12));
+	}
+}
