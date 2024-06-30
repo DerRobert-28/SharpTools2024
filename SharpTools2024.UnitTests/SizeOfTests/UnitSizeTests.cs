3ed95db928fb4e4e40e294d463434b07b5e7f216
Der Robert <erictainment@live.de>
2024-06-30 22:19:13 +0200

    implement nyte to not equal to other units tests

--- SharpTools2024.UnitTests/SizeOfTests/UnitSizeTests.cs
+++ SharpTools2024.UnitTests/SizeOfTests/UnitSizeTests.cs
@@ -100,4 +100,100 @@ public class UnitSizeTests: SizeOfHelper {
 		result = sizeOf.inTeraBytes();
 		Assert.That(result, Is.Not.EqualTo(size));
 	}
+
+	[Test]
+	public void Bytes_ShouldNotBe_Bits() {
+		var size = fetchRandomSize();
+		var sizeOf = SizeOf.Bytes(size);
+		var result = sizeOf.inBits();
+		Assert.That(result, Is.Not.EqualTo(size));
+	}
+
+	[Test]
+	public void Bytes_ShouldNotBe_KiloBits() {
+		var size = fetchRandomSize();
+		var sizeOf = SizeOf.Bytes(size);
+		var result = sizeOf.inKBits();
+		Assert.That(result, Is.Not.EqualTo(size));
+		//
+		result = sizeOf.inKiloBits();
+		Assert.That(result, Is.Not.EqualTo(size));
+	}
+
+	[Test]
+	public void Bytes_ShouldNotBe_KiloBytes() {
+		var size = fetchRandomSize();
+		var sizeOf = SizeOf.Bytes(size);
+		var result = sizeOf.inKBytes();
+		Assert.That(result, Is.Not.EqualTo(size));
+		//
+		result = sizeOf.inKiloBytes();
+		Assert.That(result, Is.Not.EqualTo(size));
+	}
+
+	[Test]
+	public void Bytes_ShouldNotBe_MegaBits() {
+		var size = fetchRandomSize();
+		var sizeOf = SizeOf.Bytes(size);
+		var result = sizeOf.inMBits();
+		Assert.That(result, Is.Not.EqualTo(size));
+		//
+		result = sizeOf.inMegaBits();
+		Assert.That(result, Is.Not.EqualTo(size));
+	}
+
+	[Test]
+	public void Bytes_ShouldNotBe_MegaBytes() {
+		var size = fetchRandomSize();
+		var sizeOf = SizeOf.Bytes(size);
+		var result = sizeOf.inMBytes();
+		Assert.That(result, Is.Not.EqualTo(size));
+		//
+		result = sizeOf.inMegaBytes();
+		Assert.That(result, Is.Not.EqualTo(size));
+	}
+
+	[Test]
+	public void Bytes_ShouldNotBe_GigaBits() {
+		var size = fetchRandomSize();
+		var sizeOf = SizeOf.Bytes(size);
+		var result = sizeOf.inGBits();
+		Assert.That(result, Is.Not.EqualTo(size));
+		//
+		result = sizeOf.inGigaBits();
+		Assert.That(result, Is.Not.EqualTo(size));
+	}
+
+	[Test]
+	public void Bytes_ShouldNotBe_GigaBytes() {
+		var size = fetchRandomSize();
+		var sizeOf = SizeOf.Bytes(size);
+		var result = sizeOf.inGBytes();
+		Assert.That(result, Is.Not.EqualTo(size));
+		//
+		result = sizeOf.inGigaBytes();
+		Assert.That(result, Is.Not.EqualTo(size));
+	}
+
+	[Test]
+	public void Bytes_ShouldNotBe_TeraBits() {
+		var size = fetchRandomSize();
+		var sizeOf = SizeOf.Bytes(size);
+		var result = sizeOf.inTBits();
+		Assert.That(result, Is.Not.EqualTo(size));
+		//
+		result = sizeOf.inTeraBits();
+		Assert.That(result, Is.Not.EqualTo(size));
+	}
+
+	[Test]
+	public void Bytes_ShouldNotBe_TeraBytes() {
+		var size = fetchRandomSize();
+		var sizeOf = SizeOf.Bytes(size);
+		var result = sizeOf.inTBytes();
+		Assert.That(result, Is.Not.EqualTo(size));
+		//
+		result = sizeOf.inTeraBytes();
+		Assert.That(result, Is.Not.EqualTo(size));
+	}
 }
